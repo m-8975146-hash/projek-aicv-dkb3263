ee12d23ca35c5feb465f8f633da8f7ccde2bc9bc
AINA <aina@kvgerik.edu.my>
2026-06-10 16:59:06 +0800

    Tambah maklumat model AI

---
+++ model_info.py
@@ -0,0 +1,7 @@
+models = {
+    "ANN": "Artificial Neural Network",
+    "CNN": "Convolutional Neural Network",
+    "YOLO": "You Only Look Once"
+}
+for kod, nama in models.items():
+    print(f"{kod}: {nama}")
